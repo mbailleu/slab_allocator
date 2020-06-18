dca8d782c506174f6d0925ecff400efc54b729fb
Maurice Bailleu <M.Bailleu@web.de>
2020-06-18 16:12:56 +0000

    made getting a copy of start and end pointer possible for munmap

--- allocator.h
+++ allocator.h
@@ -11,7 +11,6 @@ namespace slab {
 
 template<class T>
 class Allocator {
-
   T * start_region;
   T * current_start;
   T * end_region;
@@ -55,136 +54,154 @@ public:
     stack[pos] = ptr;
     ++pos;
   }
+
+  T * get_region_start() const { return start_region; }
+  T * get_region_end() const { return end_region; }
 };
 
 template<class T>
 class LockLessAllocator {
 
-    struct Node {
-      T ptr;
-      Node * prev;
-    };
+  struct Node {
+    T ptr;
+    Node * prev;
+  };
 
-    Node const * start_region;
-    Node const * end_region;
-    std::atomic<Node *> head;
-    std::atomic<Node *> next;
-    static_assert(std::atomic<Node *>::is_always_lock_free);
+  Node const * start_region;
+  Node const * end_region;
+  std::atomic<Node *> head;
+  std::atomic<Node *> next;
+  static_assert(std::atomic<Node *>::is_always_lock_free);
 
 public:
 
-    using Value_t = T;
+  using Value_t = T;
 
-    static constexpr size_t metaDataSize(size_t) {
-      return sizeof(LockLessAllocator);
+  static constexpr size_t metaDataSize(size_t) {
+    return sizeof(LockLessAllocator);
+  }
+
+  static constexpr size_t dataSize(size_t n_elements) {
+    return n_elements * sizeof(Node);
+  }
+
+  LockLessAllocator(std::byte * start, std::byte * end)
+    : start_region(reinterpret_cast<Node *>(start))
+    , end_region(reinterpret_cast<Node *>(end))
+    , head(nullptr)
+    , next(reinterpret_cast<Node *>(start)) {}
+
+  T * alloc() {
+    if (auto h = head.load(std::memory_order_relaxed); h != nullptr) {
+      while (!head.compare_exchange_weak(h, h->prev, std::memory_order_release, std::memory_order_relaxed) && h != nullptr) {}
+      if (h != nullptr) {
+        h->prev = nullptr;
+        return reinterpret_cast<T*>(h);
+      }
     }
+    Node * n = next.fetch_add(1, std::memory_order_relaxed);
+    return reinterpret_cast<T*>(n);
+  }
 
-    static constexpr size_t dataSize(size_t n_elements) {
-      return n_elements * sizeof(Node);
-    }
+  void dealloc(T * ptr) {
+    auto n = reinterpret_cast<Node *>(ptr);
+    n->prev = head.load(std::memory_order_relaxed);
+    while (!head.compare_exchange_weak(n->prev, n, std::memory_order_release, std::memory_order_relaxed)) {}
+  }
 
-    LockLessAllocator(std::byte * start, std::byte * end) : start_region(reinterpret_cast<Node *>(start)), end_region(reinterpret_cast<Node *>(end)), head(nullptr), next(reinterpret_cast<Node *>(start)) {}
-
-    T * alloc() {
-        if (auto h = head.load(std::memory_order_relaxed); h != nullptr) {
-            while (!head.compare_exchange_weak(h, h->prev, std::memory_order_release, std::memory_order_relaxed) && h != nullptr) {}
-            if (h != nullptr) {
-                h->prev = nullptr;
-                return reinterpret_cast<T*>(h);
-            }
-        }
-        Node * n = next.fetch_add(1, std::memory_order_relaxed);
-        return reinterpret_cast<T*>(n);
-    }
-
-    void dealloc(T * ptr) {
-        auto n = reinterpret_cast<Node *>(ptr);
-        n->prev = head.load(std::memory_order_relaxed);
-        while (!head.compare_exchange_weak(n->prev, n, std::memory_order_release, std::memory_order_relaxed)) {}
-    }
+  T * get_region_start() const { return start_region; }
+  T * get_region_end() const { return end_region; }
 };
 
 class DynamicLockLessAllocator {
-    std::byte * const start_region;
-    std::byte * const end_region;
+  std::byte * const start_region;
+  std::byte * const end_region;
 
-    struct Node {
-        Node * prev;
-        std::byte val[];
-    };
+  struct Node {
+    Node * prev;
+    std::byte val[];
+  };
 
-    std::atomic<Node *>      head;
-    std::atomic<std::byte *> next;
-    static_assert(std::atomic<Node *>::is_always_lock_free);
-    static_assert(std::atomic<std::byte *>::is_always_lock_free);
+  std::atomic<Node *>      head;
+  std::atomic<std::byte *> next;
+  static_assert(std::atomic<Node *>::is_always_lock_free);
+  static_assert(std::atomic<std::byte *>::is_always_lock_free);
 
-    size_t const element_size;
+  size_t const element_size;
 
 public:
 
-    using Value_t = std::byte;
+  using Value_t = std::byte;
 
-    static constexpr size_t metaDataSize(size_t) {
-        return sizeof(DynamicLockLessAllocator);
+  static constexpr size_t metaDataSize(size_t) {
+    return sizeof(DynamicLockLessAllocator);
+  }
+
+  size_t dataSize(size_t n_elements) const {
+    return n_elements * (element_size + sizeof(Node));
+  }
+
+
+
+  DynamicLockLessAllocator(std::byte * start, std::byte * end, size_t element_size)
+    : start_region(start)
+    , end_region(end)
+    , head(nullptr)
+    , next(start)
+    , element_size([] (auto x) {
+        auto remainder = x % 8;
+        if (remainder == 0) return x;
+        return x + (8 - remainder);
+      }(element_size)) {}
+
+  inline std::byte * alloc() {
+    if (auto h = head.load(std::memory_order_relaxed); h != nullptr) {
+      while (!head.compare_exchange_weak(h, h->prev, std::memory_order_release, std::memory_order_relaxed) && h != nullptr) {}
+      if (h != nullptr) {
+        h->prev = nullptr;
+        return h->val;
+      }
     }
+    auto * buf = next.fetch_add(sizeof(Node) + element_size, std::memory_order_relaxed);
+    auto n = reinterpret_cast<Node *>(buf);
+    n->prev = nullptr;
+    return n->val;
+  }
 
-    size_t dataSize(size_t n_elements) const {
-        return n_elements * (element_size + sizeof(Node));
-    }
+  void dealloc(std::byte * ptr) {
+    auto n = reinterpret_cast<Node *>(ptr - offsetof(Node, val));
+    n->prev = head.load(std::memory_order_relaxed);
+    while(!head.compare_exchange_weak(n->prev, n, std::memory_order_release, std::memory_order_relaxed)) {}
+  }
 
-
-
-    DynamicLockLessAllocator(std::byte * start, std::byte * end, size_t element_size) : start_region(start), end_region(end), head(nullptr), next(start), element_size([] (auto x) {
-              auto remainder = x % 8;
-              if (remainder == 0) return x;
-              return x + (8 - remainder);
-            }(element_size)) {}
-
-    inline std::byte * alloc() {
-        if (auto h = head.load(std::memory_order_relaxed); h != nullptr) {
-            while (!head.compare_exchange_weak(h, h->prev, std::memory_order_release, std::memory_order_relaxed) && h != nullptr) {}
-            if (h != nullptr) {
-                h->prev = nullptr;
-                return h->val;
-            }
-        }
-        auto * buf = next.fetch_add(sizeof(Node) + element_size, std::memory_order_relaxed);
-        auto n = reinterpret_cast<Node *>(buf);
-        n->prev = nullptr;
-        return n->val;
-    }
-
-    void dealloc(std::byte * ptr) {
-        auto n = reinterpret_cast<Node *>(ptr - offsetof(Node, val));
-        n->prev = head.load(std::memory_order_relaxed);
-        while(!head.compare_exchange_weak(n->prev, n, std::memory_order_release, std::memory_order_relaxed)) {}
-    }
+  std::byte * get_region_start() const { return start_region; }
+  std::byte * get_region_end() const { return end_region; }
 };
 
 template<class Allocator>
 class UniquePtrWrap : public Allocator {
 public:
-    using Value_t = typename Allocator::Value_t;
+  using Value_t = typename Allocator::Value_t;
 
-    struct Deleter {
-       Allocator * base;
-       void operator()(Value_t * ptr) {
-           base->dealloc(ptr);
-       }
-    } d;
-
-    using Ptr_t = std::unique_ptr<Value_t, Deleter>;
-
-    template<class ... Args>
-    UniquePtrWrap(Args && ... args) : Allocator(std::forward<Args>(args)...), d({this}) {}
-
-    Ptr_t alloc() {
-        return {Allocator::alloc(), d};
+  struct Deleter {
+    Allocator * base;
+    void operator()(Value_t * ptr) {
+      base->dealloc(ptr);
     }
+  } d;
 
-    void dealloc(Ptr_t value) {
-        value.reset();
-    }
+  using Ptr_t = std::unique_ptr<Value_t, Deleter>;
+
+  template<class ... Args>
+  UniquePtrWrap(Args && ... args) : Allocator(std::forward<Args>(args)...), d({this}) {}
+
+  Ptr_t alloc() {
+    return {Allocator::alloc(), d};
+  }
+
+  void dealloc(Ptr_t value) {
+    value.reset();
+  }
 };
 
 } //namespace buddy
